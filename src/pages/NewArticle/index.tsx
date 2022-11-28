04980d9daab64503805937df974f3a57057496ca
jayce <jaycethanks@outlook.com>
2022-11-28 23:24:24 +0800

    feat: 引入 bytemd markdown 编辑器

--- src/pages/NewArticle/index.tsx
+++ src/pages/NewArticle/index.tsx
@@ -1,3 +1,27 @@
+import gfm from '@bytemd/plugin-gfm';
+import { useState } from 'react';
+import { Editor, Viewer } from '@bytemd/react';
+import 'bytemd/dist/index.css';
+import styles from './index.module.less';
+console.log('[styles]: ', styles);
+const plugins = [
+  gfm(),
+  // Add more plugins here
+];
+
 export default () => {
-  return <div>new article</div>;
+  const [value, setValue] = useState('');
+
+  return (
+    <div className={styles.main}>
+      <Editor
+        className={styles.bytemd}
+        value={value}
+        plugins={plugins}
+        onChange={(v) => {
+          setValue(v);
+        }}
+      />
+    </div>
+  );
 };
